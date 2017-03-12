d4552b240372cef463aaffc7d6e299d4fba38027
Mohamed Sahad KP <mohamed.sahad@experionglobal.com>
2017-03-12 18:01:25 +0530

    updated api

--- js/submit.js
+++ js/submit.js
@@ -1,7 +1,7 @@
 (function($){
     function processForm( e ){
         $.ajax({
-            url: 'https://demo.com/api/',
+            url: 'https://b8spu1pxd1.execute-api.us-east-1.amazonaws.com/prod/api/',
             type: 'POST',
             contentType: 'application/json',
             data: JSON.stringify( { "name": $('#name').val(), "email": $('#email').val(), "message": $('#message').val() } ),
