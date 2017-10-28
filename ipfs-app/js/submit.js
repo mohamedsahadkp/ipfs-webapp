c00abbafc31534619371dfbcb9ceccad9c975c95
Mohamed Sahad KP <mohamed.sahad@experionglobal.com>
2017-10-28 18:02:02 +0530

    updated ipfs app

--- ipfs-app/js/submit.js
+++ ipfs-app/js/submit.js
@@ -1,7 +1,7 @@
 (function($){
     function processForm( e ){
         $.ajax({
-            url: 'https://b8spu1pxd1.execute-api.us-east-1.amazonaws.com/prod/api/',
+            url: 'https://www.demo.com/api/',
             type: 'POST',
             contentType: 'application/json',
             data: JSON.stringify( { "name": $('#name').val(), "email": $('#email').val(), "message": $('#message').val() } ),
